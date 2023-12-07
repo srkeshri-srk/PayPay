22d46b7949f70fada7fdd6426e867e05c60d1c91
Shreyansh <Srkeshri.srk@gmail.com>
2023-12-07 14:35:45 +0530

    Nav title

--- PayPay/PayPay/Modules/Home/Views/HomeViewController.swift
+++ PayPay/PayPay/Modules/Home/Views/HomeViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class HomeViewController: BaseViewController {
         
+    @IBOutlet weak var swapButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -17,6 +19,18 @@ class HomeViewController: BaseViewController {
     
     private func setupUI() {
         setNavBar(title: Constants.Home.navBarTitle, prefersLargeTitles: true)
+        swapButton.layer.cornerRadius = 5.0
+        swapButton.layer.masksToBounds = true
+    }
+    
+    @IBAction func swapButtonAction(_ sender: UIButton) {
+        UIView.animate(withDuration: 0.3) {
+            if sender.transform == .identity {
+                self.swapButton.transform = CGAffineTransform(rotationAngle: .pi)
+            } else {
+                self.swapButton.transform = .identity
+            }
+        }
     }
         
 }
